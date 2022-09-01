b739d20b282f225d44898954d21c3f09a41f6b70
kevisoncb <kevisonc.brandes@gmail.com>
2022-09-01 16:22:53 -0300

    Erro corrigido.

--- Scripts SQL/insert fornecedor.sql
+++ Scripts SQL/insert fornecedor.sql
@@ -4,7 +4,7 @@
   ('Retro Visor', 'Distribuidora de Retrovisores', 13266572000169, 322370949), #retrovisor
   ('Lanternas FX', 'Lanternas FX Serviços e Distruibuição', 85987299000172, 898313821), #lanternas
   ('FlashFarois', 'FlashFarois Comércio e Industria LTDA', 746564845979, 407516646), #farol
-  ('Controle de Lanternagem', 'Controle de Lanternagem Serviço', 85987299000172, 138338884), #lanternas
+  ('Controle de Lanternagem', 'Controle de Lanternagem Serviço', 59450336000107, 138338884), #lanternas
   ('All Vidros', 'All Vidros Industria e Serviços', 27648252000188, 127545760), #vidro
   ('AutoFaróis', 'AutoFaróis Distribuidora', 82003008000176, 244519021), #farol
   ('Industria do Parachoque', 'Industria do Parachoque Industria e Comércio', 11280694000184, 437341844), #parachoque
