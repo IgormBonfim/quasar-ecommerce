1a732f366fbea63bc0bdfa23c6d12a4d9064cfbb
liviamaria <livia_teixeira@outlook.com.br>
2022-09-06 16:34:06 -0300

    Criando NHibernate

--- quasar/Quasar.API/Program.cs
+++ quasar/Quasar.API/Program.cs
@@ -1,3 +1,7 @@
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISessionFactory>(factory =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MySql");
+    return Fluently.Configure().Database(MySQLConfiguration.Standard
+                                            .ConnectionString(connectionString)
+                                            .FormatSql()
+                                            .ShowSql())
+                                            .BuildSessionFactory();
+});
 
 var app = builder.Build();
 
