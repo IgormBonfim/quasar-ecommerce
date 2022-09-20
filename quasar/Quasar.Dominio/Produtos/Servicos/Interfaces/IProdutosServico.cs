dbacd52100fbf13e1b1c7a377019eb9dbb6bda81
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-20 16:03:06 -0300

    Adiciona Interface de Servicos de Produto

---
+++ quasar/Quasar.Dominio/Produtos/Servicos/Interfaces/IProdutosServico.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quasar.Dominio.Categorias.Entidades;
+using Quasar.Dominio.Fornecedores.Entidades;
+using Quasar.Dominio.Produtos.Entidades;
+
+namespace Quasar.Dominio.Produtos.Servicos.Interfaces
+{
+    public interface IProdutosServico
+    {
+        Produto Validar(int id);
+        Produto Inserir(Produto produto);
+        Produto Instanciar(string? descricaoProduto, string? nomeProduto, Categoria? categoria, Fornecedor? fornecedor, string? imgPrincipalProduto, string? imgSegundaProduto, string? imgTerceiraProduto);
+        Produto Editar(Produto produto);
+        void Deletar(Produto produto);
+    }
+}
