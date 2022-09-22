157eeb074a4ec634477fa52c009e6b0696ca9e67
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-22 15:13:07 -0300

    Corrige metodo Editar para receber o id do produto ao inves do Produto

--- quasar/Quasar.Dominio/Produtos/Servicos/Interfaces/IProdutosServico.cs
+++ quasar/Quasar.Dominio/Produtos/Servicos/Interfaces/IProdutosServico.cs
@@ -14,6 +14,6 @@ namespace Quasar.Dominio.Produtos.Servicos.Interfaces
         Produto Inserir(Produto produto);
         Produto Instanciar(string? descricaoProduto, string? nomeProduto, Categoria? categoria, Fornecedor? fornecedor, string? imgPrincipalProduto, string? imgSegundaProduto, string? imgTerceiraProduto);
         Produto Editar(Produto produto);
-        void Deletar(Produto produto);
+        void Deletar(int idProduto);
     }
 }
