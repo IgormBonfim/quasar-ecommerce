deff7bbfefb548b9754725bc5e71acce9496277d
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-23 16:47:14 -0300

    Adiciona ProdutoResponse em DataTransfer

---
+++ quasar/Quasar.DataTransfer/Produtos/Responses/ProdutoResponse.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.DataTransfer.Produtos.Responses
+{
+    public class ProdutoResponse
+    {
+        public int IdProduto { get; set; }
+        public string DescricaoProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public string ImgPrincipalProduto { get; set; }
+
+    }
+}
