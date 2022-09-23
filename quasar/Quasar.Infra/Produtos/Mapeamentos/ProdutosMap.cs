167c58c7c1e4c930a553f09f1e4f72d3e51a3492
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-23 14:51:19 -0300

    Adiciona mapeamento de Produto

---
+++ quasar/Quasar.Infra/Produtos/Mapeamentos/ProdutosMap.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using Quasar.Dominio.Produtos.Entidades;
+
+namespace Quasar.Infra.Produtos.Mapeamentos
+{
+    public class ProdutosMap : ClassMap<Produto>
+    {
+        public ProdutosMap()
+        {
+            Schema("quasarecommerce");
+            Table("produto");
+            Id(p => p.IdProduto).Column("idProduto").GeneratedBy.Identity();
+            Map(p => p.DescricaoProduto).Column("descricaoProduto");
+            Map(p => p.NomeProduto).Column("nomeProduto");
+            Map(p => p.ImgPrincipalProduto).Column("imgProduto");
+            References(p => p.Categoria).Column("idCategoria");
+            References(p => p.Fornecedor).Column("idFornecedor");
+            
+            //References - Relacionamento um para um
+            //HasMany - Relacionamento um para N
+            //HasManyToMany - Relacionamento N para N
+        }
+        
+    }
+}
