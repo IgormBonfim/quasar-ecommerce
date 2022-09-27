ee68af8f45e76966a77f6aee49ac9b93ed6c4114
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-27 16:14:12 -0300

    Adiciona requests e responses de produto inserir

---
+++ quasar/Quasar.DataTransfer/Produtos/Responses/ProdutoInserirResponse.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.DataTransfer.Produtos.Responses
+{
+    public class ProdutoInserirResponse
+    {
+        public int IdProduto { get; set; } 
+        public string NomeProduto { get; set; }
+    }
+}
