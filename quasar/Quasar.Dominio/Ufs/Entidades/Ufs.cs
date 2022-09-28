4d0cec221c9dd522b7c5fa0f124eb4a4d1c4831f
kevisoncb <kevisonc.brandes@gmail.com>
2022-09-28 17:46:19 -0300

    Criando os metodes de UF.

---
+++ quasar/Quasar.Dominio/Ufs/Entidades/Ufs.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.Dominio.Ufs.Entidades
+{
+    public class Ufs
+    {
+        
+
+        public virtual int IdUf { get; protected set; }
+        public virtual string SiglaUf { get; protected set; }
+        public virtual  string DescUf { get; protected set; }
+
+        public Ufs(){ }
+
+        public Ufs(int idUf, string siglaUf, string descUf)
+        {
+            SetIdUf(idUf);
+            SetSiglaUf(siglaUf);
+            SetDescUf(descUf);
+        }
+
+        public virtual void SetIdUf(int? idUf)
+        {
+            if(!idUf.HasValue)
+            throw new Exception("O código da UF tem que ser obrigatório!");
+            IdUf = idUf.Value;
+        }
+        public virtual void SetSiglaUf(string siglaUf)
+        {
+            throw new Exception("A sigla da UF tem que ser obrigatório!");
+            SiglaUf = siglaUf;
+        }
+        public virtual void SetDescUf(string descUf)
+        {
+            throw new Exception("A descrição da UF tem que ser obrigatório!");
+            DescUf = descUf;
+        }
+    }
+}
