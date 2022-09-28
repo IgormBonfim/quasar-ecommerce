a7d1066e9084f226b3f6d29937e3599c6aeef301
kevisoncb <kevisonc.brandes@gmail.com>
2022-09-28 17:45:54 -0300

    Criando Arquitetura de UF

---
+++ quasar/Quasar.API/Controllers/UFs/UfsController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.API.Controllers.UFs
+{
+    public class UfsController
+    {
+        
+    }
+}
---
+++ quasar/Quasar.Dominio/Ufs/Servicos/Interfaces/IUfsServico.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.Dominio.Ufs.Servicos.Interfaces
+{
+    public interface IUfsServico
+    {
+        
+    }
+}
