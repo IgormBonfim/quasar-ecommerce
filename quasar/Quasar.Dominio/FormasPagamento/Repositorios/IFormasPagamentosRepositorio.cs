e79d40473e8f2a1de5056720a69b151b70d44ade
Igor Bonfim <igormbonfim@hotmail.com>
2022-09-28 14:14:04 -0300

    Adiciona Interface de repositorio de FormaPagamento

---
+++ quasar/Quasar.Dominio/FormasPagamento/Repositorios/IFormasPagamentosRepositorio.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quasar.Dominio.FormasPagamento.Entidades;
+
+namespace Quasar.Dominio.FormasPagamento.Repositorios
+{
+    public interface IFormasPagamentoRepositorio
+    {
+        FormaPagamento Recuperar(int id);
+    }
+}
