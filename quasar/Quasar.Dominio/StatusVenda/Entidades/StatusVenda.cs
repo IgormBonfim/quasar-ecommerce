3175628a632e9737526fe95cf2a1ad370ebb3f72
liviamaria <livia_teixeira@outlook.com.br>
2022-09-29 13:22:19 -0300

    Criação da classe StatusVenda, criação das propriedades e criação dos métodos Sets.

--- quasar/Quasar.Dominio/StatusVenda/Entidades/StatusVenda.cs
+++ quasar/Quasar.Dominio/StatusVenda/Entidades/StatusVenda.cs
@@ -7,6 +7,36 @@ namespace Quasar.Dominio.StatusVenda.Entidades
 {
     public class StatusVenda
     {
-        
+        public virtual int IdStatusVenda { get; protected set; }
+        public virtual string DescricaoStatusVenda { get; protected set; }
+
+        public StatusVenda () {}
+
+        public StatusVenda(string? DescricaoStatusVenda)
+        {
+            SetDescricaoStatusVenda(DescricaoStatusVenda);
+        }
+
+        public virtual void SetIdStatusVenda (int? idStatusVenda)
+        {
+            if(!idStatusVenda.HasValue)
+            {
+                throw new Exception("Código do Status da Venda é obrigatório!");
+            }
+            IdStatusVenda = idStatusVenda.Value;
+        }
+
+        public virtual void SetDescricaoStatusVenda (string? descricaoStatusVenda)
+        {
+            if(string.IsNullOrWhiteSpace(descricaoStatusVenda) && descricaoStatusVenda.Length < 20)
+                throw new Exception("O campo descrição deve possuir ao menos 20 caracteres!");
+
+            if(descricaoStatusVenda.Length > 50)
+                throw new Exception("O campo descrição deve possuir até 50 caracteres!");
+
+            DescricaoStatusVenda = descricaoStatusVenda;
+        }
+
     }
 }
+
