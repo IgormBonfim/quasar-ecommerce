2f1a8f136e36d0d9d4a483239e75d103aeaabff1
Aline_Kato <alineamy1612@gmail.com>
2022-09-30 14:56:43 -0300

    Arquivo Request de CategoriaInserir da DataTransfer

---
+++ quasar/Quasar.DataTransfer/Categorias/Requests/CategoriaInserirRequest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.DataTransfer.Categorias.Requests
+{
+    public class CategoriaInserirRequest
+    {
+        public string NomeCategoria {get; set;}
+        public string ImgCategoria {get; set;}
+    }
+}
