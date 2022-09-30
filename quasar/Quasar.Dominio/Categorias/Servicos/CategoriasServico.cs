5751f0f2d7040f2dbbbb7ad6a797d6edb4aed913
Aline_Kato <alineamy1612@gmail.com>
2022-09-30 15:01:17 -0300

    Arquivo CategoriasServico do Dominio

---
+++ quasar/Quasar.Dominio/Categorias/Servicos/CategoriasServico.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quasar.Dominio.Categorias.Entidades;
+using Quasar.Dominio.Categorias.Repositorios;
+using Quasar.Dominio.Categorias.Servicos.Interfaces;
+
+namespace Quasar.Dominio.Categorias.Servicos
+{
+    public class CategoriasServico : ICategoriasServico
+    {
+        private readonly ICategoriasRepositorio categoriasRepositorio;
+
+        public IList<Categoria> Buscar(IQueryable<Categoria> query)
+        {
+            return query.ToList();
+        }
+
+        public void Deletar(int idCategoria)
+        {
+            Categoria categoriaDeletar = Validar(idCategoria);
+            categoriasRepositorio.Deletar(categoriaDeletar);
+        }
+
+        public Categoria Editar(Categoria categoria)
+        {
+            Categoria categoriaEditar = Validar(categoria.IdCategoria);
+
+            if(categoria.NomeCategoria != categoriaEditar.NomeCategoria);
+            categoriaEditar.SetNomeCategoria(categoria.NomeCategoria);
+
+            if(categoria.ImgCategoria != categoriaEditar.ImgCategoria);
+            categoriaEditar.SetImgCategoria(categoria.ImgCategoria);
+
+            return categoriasRepositorio.Editar(categoriaEditar);
+        }
+
+        public Categoria Inserir(Categoria categoria)
+        {
+            return categoriasRepositorio.Inserir(categoria);
+        }
+
+        public Categoria Instanciar(string nomeCategoria, string imgCategoria)
+        {
+            Categoria categoria = new Categoria(nomeCategoria, imgCategoria);
+            return categoria;
+        }
+
+        public Categoria Validar(int id)
+        {
+            Categoria categoriaValidar = categoriasRepositorio.Recuperar(id);
+            if(categoriaValidar == null)
+                throw new Exception ("Categoria não encontrado");
+            return categoriaValidar;
+        }
+    }
+}
