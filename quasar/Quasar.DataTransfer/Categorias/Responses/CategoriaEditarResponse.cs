0e2cd980f6043159bb214e1180ff0043b56e601f
Aline_Kato <alineamy1612@gmail.com>
2022-09-30 14:58:34 -0300

    Arquivo Response de CategoriaEditar da DataTransfer

---
+++ quasar/Quasar.DataTransfer/Categorias/Responses/CategoriaEditarResponse.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.DataTransfer.Categorias.Responses
+{
+    public class CategoriaEditarResponse
+    {
+        public int IdCategoria {get; set;}
+        public string NomeCategoria {get; set;}
+    }
+}
