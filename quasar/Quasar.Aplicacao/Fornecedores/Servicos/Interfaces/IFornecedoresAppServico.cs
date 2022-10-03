7eb729662bec1098815e28402041fe6b58f2e4d8
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-10-03 17:33:07 -0300

    Adicionando a interface de fornecedores app servico

--- quasar/Quasar.Aplicacao/Fornecedores/Servicos/Interfaces/IFornecedoresAppServico.cs
+++ quasar/Quasar.Aplicacao/Fornecedores/Servicos/Interfaces/IFornecedoresAppServico.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Quasar.DataTransfer.Fornecedores.Requests;
 using Quasar.DataTransfer.Fornecedores.Responses;
 
 namespace Quasar.Aplicacao.Fornecedores.Servicos.Interfaces
 {
     public interface IFornecedoresAppServico
     {
-        FornecedorResponse Recuperar(int idFornecedor);
-        FornecedorInserirResponse Inserir(FornecedorInserirRequest fornecedorInserirRequest);
+        FornecedorResponse Recuperar(int id);
+        FornecedorInserirResponse Inserir(FornecedorInserirRequest inserirRequest);
+        FornecedorEditarResponse Editar(FornecedorEditarRequest editarRequest);
+        IList<FornecedorResponse> Buscar (CategoriaBuscarRequest BuscarRequest);
+        void Deletar (int id);
     }
 }
