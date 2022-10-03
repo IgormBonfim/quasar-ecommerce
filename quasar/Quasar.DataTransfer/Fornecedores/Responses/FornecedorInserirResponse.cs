5fab3460a6b8fe5687128316f67cc4e8f8283f1f
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-10-03 17:34:01 -0300

    Adicionando os Responses e Requests na camada de data transfer (Não implementadas)

---
+++ quasar/Quasar.DataTransfer/Fornecedores/Responses/FornecedorInserirResponse.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.DataTransfer.Fornecedores.Responses
+{
+    public class FornecedorInserirResponse
+    {
+        
+    }
+}
