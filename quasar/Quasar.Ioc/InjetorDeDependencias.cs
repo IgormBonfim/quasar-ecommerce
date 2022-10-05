51abbce8e9fb977a954832d62b5451bc09e7eeb2
Igor Bonfim <igormbonfim@hotmail.com>
2022-10-05 15:16:06 -0300

    Adiciona camada de Ioc com metodo InjetarDependencias

---
+++ quasar/Quasar.Ioc/InjetorDeDependencias.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.Ioc
+{
+    public static class InjetorDeDependencias
+    {
+        public static void InjetarDependencias(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
