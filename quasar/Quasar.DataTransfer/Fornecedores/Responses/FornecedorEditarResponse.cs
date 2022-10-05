9b059c4b2c121435d1996cd4ceaee1bb71f9c6f6
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-10-05 14:30:20 -0300

    Alterando Respnses de editar e inserir

--- quasar/Quasar.DataTransfer/Fornecedores/Responses/FornecedorEditarResponse.cs
+++ quasar/Quasar.DataTransfer/Fornecedores/Responses/FornecedorEditarResponse.cs
@@ -7,10 +7,10 @@ namespace Quasar.DataTransfer.Fornecedores.Responses
 {
     public class FornecedorEditarResponse
     {
-        public int? IdFornecedor { get; set; }
-        public string? NomeFornecedor { get; set; }
-        public string? RazaoSocialFornecedor { get; set; }
-        public string? CnpjFornecedor { get; set; }
+        public int IdFornecedor { get; set; }
+        public string NomeFornecedor { get; set; }
+        public string RazaoSocialFornecedor { get; set; }
+        public string CnpjFornecedor { get; set; }
         public string? IeFornecedor { get; set; }
     }
 }
