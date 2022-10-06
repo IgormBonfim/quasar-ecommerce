728f709df88114c831bc8099b4394f3d55412397
Igor Bonfim <igormbonfim@hotmail.com>
2022-10-06 17:22:06 -0300

    Remove usings desnecessarios de program

--- quasar/Quasar.API/Program.cs
+++ quasar/Quasar.API/Program.cs
@@ -1,45 +1,3 @@
-using FluentNHibernate.Cfg;
-using FluentNHibernate.Cfg.Db;
-using NHibernate;
-using Quasar.Aplicacao.Categorias.Servicos;
-using Quasar.Aplicacao.Categorias.Servicos.Interfaces;
-using Quasar.Aplicacao.Produtos.Profiles;
-using Quasar.Aplicacao.Produtos.Servicos;
-using Quasar.Aplicacao.Produtos.Servicos.Interfaces;
-using Quasar.Dominio.Categorias.Repositorios;
-using Quasar.Dominio.Categorias.Servicos;
-using Quasar.Dominio.Categorias.Servicos.Interfaces;
-using Quasar.Dominio.Produtos.Repositorios;
-using Quasar.Dominio.Produtos.Servicos;
-using Quasar.Dominio.Produtos.Servicos.Interfaces;
-using Quasar.Infra.Categorias;
-using Quasar.Aplicacao.FormasPagamento.Servicos;
-using Quasar.Aplicacao.FormasPagamento.Servicos.Interfaces;
-using Quasar.Aplicacao.Ufs.Servicos;
-using Quasar.Aplicacao.Ufs.Servicos.Interfaces;
-using Quasar.Dominio.Ufs.Repositorios;
-using Quasar.Dominio.Ufs.Servicos;
-using Quasar.Dominio.Ufs.Servicos.Interfaces;
-using Quasar.Dominio.FormasPagamento.Repositorios;
-using Quasar.Dominio.FormasPagamento.Servicos;
-using Quasar.Dominio.FormasPagamento.Servicos.Interfaces;
-using Quasar.Aplicacao.Fornecedores.Servicos;
-using Quasar.Aplicacao.Fornecedores.Servicos.Interfaces;
-using Quasar.Dominio.Fornecedores.Repositorios;
-using Quasar.Dominio.Fornecedores.Servicos;
-using Quasar.Dominio.Fornecedores.Servicos.Interfaces;
-using Quasar.Infra.FormasPagamento;
-using Quasar.Infra.Fornecedores;
-using Quasar.Infra.Fornecedores.Mapeamentos;
-using Quasar.Aplicacao.StatusVendas.Servicos;
-using Quasar.Aplicacao.StatusVendas.Servicos.Interfaces;
-using Quasar.Dominio.StatusVendas.Repositorios;
-using Quasar.Dominio.StatusVendas.Servicos;
-using Quasar.Dominio.StatusVendas.Servicos.Interfaces;
-using Quasar.Infra.Produtos;
-using Quasar.Infra.StatusVendas;
-using Quasar.Infra.Ufs;
-using ISession = NHibernate.ISession;
 using Quasar.Ioc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -52,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.InjetarDependencias(builder.Configuration);
+
 
 var app = builder.Build();
 
