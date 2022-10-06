441059d654f04459fe52867eb2d7e7d111e7b40f
kevisoncb <kevison.brande@autoglass.com.br>
2022-10-06 17:15:44 -0300

    Criei todas as pastas designadas para o estoque.

---
+++ quasar/Quasar.Dominio/Estoques/Servicos/Interfaces/IEstoquesServicos.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.Dominio.Estoques.Servicos.Interfaces
+{
+    public interface IEstoquesServicos
+    {
+        
+    }
+}
