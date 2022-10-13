671c74cb697362fc17a68ac0a4495504e44b47e0
kevisoncb <kevison.brande@autoglass.com.br>
2022-10-13 14:49:58 -0300

    Criando Profile e AppServico

--- quasar/Quasar.Aplicacao/Estoques/Profiles/EstoquesProfile.cs
+++ quasar/Quasar.Aplicacao/Estoques/Profiles/EstoquesProfile.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Quasar.DataTransfer.Estoques.Responses;
+using Quasar.Dominio.Estoques.Entidades;
 
 namespace Quasar.Aplicacao.Estoques.Profiles
 {
-    public class EstoquesProfile
+    public class EstoquesProfile : Profile
     {
-        
+        public EstoquesProfile()
+        {
+            CreateMap<Estoque, EstoqueInserirResponse>();
+        }
     }
 }
