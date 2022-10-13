587464210077d7aed6a003f1e6d65cd00513bb50
Aline_Kato <alineamy1612@gmail.com>
2022-10-13 14:24:30 -0300

    Insert Categorias

--- Scripts SQL/InsertCategorias.sql
+++ Scripts SQL/InsertCategorias.sql
@@ -1,8 +1,6 @@
-  INSERT INTO categoria(nomeCategoria, imgCategoria)VALUES
-  (
+INSERT INTO categoria(nomeCategoria, imgCategoria)VALUES
 	('Vidro','https://raw.githubusercontent.com/IgormBonfim/quasar-media/main/Categorias/VidroCategorias.png'),
     ('Farol','https://raw.githubusercontent.com/IgormBonfim/quasar-media/main/Categorias/FarolCategorias.png'),
     ('Lanterna','https://raw.githubusercontent.com/IgormBonfim/quasar-media/main/Categorias/LanternaCategorias.png'),
     ('Retrovisor','https://raw.githubusercontent.com/IgormBonfim/quasar-media/main/Categorias/RetrovisorCategorias.png'),
-    ('Para-Choque','https://raw.githubusercontent.com/IgormBonfim/quasar-media/main/Categorias/ParachoqueCategorias.png')
-  );
+    ('Para-Choque','https://raw.githubusercontent.com/IgormBonfim/quasar-media/main/Categorias/ParachoqueCategorias.png');
