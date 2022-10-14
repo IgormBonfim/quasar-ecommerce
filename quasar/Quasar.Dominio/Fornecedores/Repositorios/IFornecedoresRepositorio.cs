dcce5df2967d475d2629d801e3bc42c1aa9e3654
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-10-14 17:07:36 -0300

    Adaptando Fornecedor para utilizar o metodo generico

--- quasar/Quasar.Dominio/Fornecedores/Repositorios/IFornecedoresRepositorio.cs
+++ quasar/Quasar.Dominio/Fornecedores/Repositorios/IFornecedoresRepositorio.cs
@@ -3,15 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Quasar.Dominio.Fornecedores.Entidades;
+using Quasar.Dominio.Genericos;
 
 namespace Quasar.Dominio.Fornecedores.Repositorios
 {
-    public interface IFornecedoresRepositorio
-    {
-        Fornecedor Inserir(Fornecedor fornecedor);
-        Fornecedor Editar(Fornecedor fornecedor);
-        void Deletar(Fornecedor fornecedor);
-        Fornecedor Recuperar(int codigo);
-        IQueryable<Fornecedor> Query();
-    }
+    public interface IFornecedoresRepositorio : IGenericosRepositorio<Fornecedor>
+    {}
 }
