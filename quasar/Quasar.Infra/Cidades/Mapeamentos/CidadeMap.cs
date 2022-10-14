5c4242b15e202630749214b992b4a55cb5506196
Livia Pin <liviamtp@icloud.com>
2022-10-14 14:50:11 -0300

    Criação da classe CidadeMap, com o mapeamento do banco de dados.

---
+++ quasar/Quasar.Infra/Cidades/Mapeamentos/CidadeMap.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using Quasar.Dominio.Cidades.Entidades;
+
+namespace Quasar.Infra.Cidades.Mapeamentos
+{
+    public class CidadeMap : ClassMap<Cidade>
+    {
+        public CidadeMap()
+        {
+            Schema("quasarecommerce");
+            Table("cidade");
+            Id(p => p.Codigo).Column("codCidade").GeneratedBy.Identity();
+            Map(p => p.Nome).Column("Nome");
+            References(p => p.Uf).Column("codUf");
+        }
+    }
+}
