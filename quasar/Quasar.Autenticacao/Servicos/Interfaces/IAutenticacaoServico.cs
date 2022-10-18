3e0574f026213479c7052d33fb2eebf700318351
Igor Bonfim <igormbonfim@hotmail.com>
2022-10-18 14:51:48 -0300

    Adiciona Interface de servico de Autenticacao

--- quasar/Quasar.Autenticacao/Servicos/Interfaces/IAutenticacaoServico.cs
+++ quasar/Quasar.Autenticacao/Servicos/Interfaces/IAutenticacaoServico.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Quasar.DataTransfer.Usuarios.Requests;
+using Quasar.DataTransfer.Usuarios.Responses;
 
 namespace Quasar.Autenticacao.Servicos.Interfaces
 {
     public interface IAutenticacaoServico
     {
-        
+        Task<UsuarioCadastroResponse> Cadastrar(UsuarioCadastroRequest cadastroRequest);
+        Task<UsuarioLoginResponse> Login(UsuarioLoginRequest loginRequest);
     }
 }
