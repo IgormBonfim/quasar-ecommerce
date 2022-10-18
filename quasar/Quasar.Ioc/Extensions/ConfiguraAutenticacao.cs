af931d38fe2505358d7594aaf8e93c72d428235a
Igor Bonfim <igormbonfim@hotmail.com>
2022-10-18 16:48:22 -0300

    Adiciona metodo de extensão de configuração de autenticacao

---
+++ quasar/Quasar.Ioc/Extensions/ConfiguraAutenticacao.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Quasar.Autenticacao.Configuracoes;
+
+namespace Quasar.Ioc.Extensions
+{
+    public static class ConfiguraAutenticacao
+    {
+        public static void AddAutenticacao(this IServiceCollection services, IConfiguration configuration)
+        {
+            IConfigurationSection jwtAppSettingOptions = configuration.GetSection(nameof(JwtOptions));
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(configuration.GetSection("JwtOptions:SecurityKey").Value));
+
+            services.Configure<JwtOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
+                options.Expiration = int.Parse(jwtAppSettingOptions[nameof(JwtOptions.Expiration)] ?? "0");
+            });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.SignIn.RequireConfirmedEmail = true;
+            });
+
+            TokenValidationParameters tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = configuration.GetSection("JwtOptions:Issuer").Value,
+
+                ValidateAudience = true,
+                ValidAudience = configuration.GetSection("JwtOptions:Audience").Value,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = securityKey,
+
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = tokenValidationParameters;
+            });
+        }
+    }
+}
