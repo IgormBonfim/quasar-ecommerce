0db6702be1fa51c2873c1553862969edad174c6c
Livia Pin <livia.pin@autoglass.com.br>
2022-10-18 14:15:35 -0300

    Adaptando Cidades para utilizar o metodo generico.

--- quasar/Quasar.Dominio/Cidades/Repositorios/ICidadesRepositorio.cs
+++ quasar/Quasar.Dominio/Cidades/Repositorios/ICidadesRepositorio.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Quasar.Dominio.Cidades.Entidades;
+using Quasar.Dominio.Genericos;
 
 namespace Quasar.Dominio.Cidades.Repositorios
 {
-    public interface ICidadesRepositorio
-    {
-        Cidade Recuperar(int codigo);
-        IQueryable<Cidade> Query();  
-    }
+    public interface ICidadesRepositorio : IGenericosRepositorio<Cidade> {}
 }
