d726dd7cdd11132215f10ba88d03e02628713b22
Livia Pin <livia.pin@autoglass.com.br>
2022-10-18 14:46:17 -0300

    Adiona um valor no insert de Status Venda.

--- Scripts SQL/Insert StatusVenda.sql
+++ Scripts SQL/Insert StatusVenda.sql
@@ -1,8 +1,9 @@
 INSERT INTO quasarecommerce.STATUSVENDA (descricao)
-VALUES (Aguardando pagamento),
-(Pagamento aprovado),
-(Pedido enviado),
-(Cancelado),
-(Pagamento recusado),
-(Finalizado)
+VALUES ("Aguardando pagamento"),
+("Pagamento aprovado"),
+("Pedido enviado"),
+("Cancelado"),
+("Pagamento recusado"),
+("Pedido em separação"),
+("Finalizado")
 ;
