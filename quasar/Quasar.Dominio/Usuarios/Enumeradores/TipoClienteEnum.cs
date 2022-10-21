18ba35c2b56424e5332004e32c5d1f9000dd76da
Igor Bonfim <igormbonfim@hotmail.com>
2022-10-21 12:08:08 -0300

    Adiciona Enum do tipo de Cliente

---
+++ quasar/Quasar.Dominio/Usuarios/Enumeradores/TipoClienteEnum.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.Dominio.Usuarios.Enumeradores
+{
+    public enum TipoClienteEnum
+    {
+        Fisico = 0,
+        Juridico = 1,
+    }
+}
