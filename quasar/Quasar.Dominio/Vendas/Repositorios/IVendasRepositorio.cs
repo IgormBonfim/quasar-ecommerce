d637a6e9a63ca90d5d0e453b8223ab2cf88c5145
Kevison Brandes <kevison.brande@autoglass.com.br>
2022-10-24 15:49:57 -0300

    Adicionando repositorio e mapeamento

--- quasar/Quasar.Dominio/Vendas/Repositorios/IVendasRepositorio.cs
+++ quasar/Quasar.Dominio/Vendas/Repositorios/IVendasRepositorio.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Quasar.Dominio.Genericos;
+using Quasar.Dominio.Vendas.Entidades;
 
 namespace Quasar.Dominio.Vendas.Repositorios
 {
-    public interface IVendasRepositorio
-    {
-        
+    public interface IVendasRepositorio : IGenericosRepositorio<Venda>
+    {   
     }
 }
