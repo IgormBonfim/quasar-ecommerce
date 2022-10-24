88d19405fc27673c0db87312daac796ed748e52a
Kevison Brandes <kevison.brande@autoglass.com.br>
2022-10-24 15:49:57 -0300

    Adicionando repositorio e mapeamento

--- quasar/Quasar.Infra/Vendas/Mapeamentos/VendasMap.cs
+++ quasar/Quasar.Infra/Vendas/Mapeamentos/VendasMap.cs
@@ -2,11 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using Quasar.Dominio.Vendas.Entidades;
 
 namespace Quasar.Infra.Vendas.Mapeamentos
 {
-    public class VendasMap
+    public class VendasMap : ClassMap<Venda>
     {
-        
+        public VendasMap()
+        {
+            Schema("quasarecommerce");
+            Table("venda");
+            Id(p => p.Codigo).Column("codVenda").GeneratedBy.Identity();
+
+            References(p => p.StatusVenda).Column("codStatusVenda");
+            References(p => p.FormaPagamento).Column("codFormaPagamento");
+            References(p => p.Endereco).Column("codEndereco");
+            References(p => p.Usuario).Column("codUsuario");
+        }
     }
 }
