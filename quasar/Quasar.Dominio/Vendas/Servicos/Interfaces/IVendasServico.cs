cdae8e16f179528812e98e17ca64918fbbf0704e
Kevison Brandes <kevison.brande@autoglass.com.br>
2022-10-24 15:50:12 -0300

    Criando interface de vendas servico

--- quasar/Quasar.Dominio/Vendas/Servicos/Interfaces/IVendasServico.cs
+++ quasar/Quasar.Dominio/Vendas/Servicos/Interfaces/IVendasServico.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Quasar.Dominio.Produtos.Entidades;
+using Quasar.Dominio.Vendas.Entidades;
 
 namespace Quasar.Dominio.Vendas.Servicos.Interfaces
 {
     public interface IVendasServico
     {
-        
+        Venda Validar(int codigo);
+        Venda Inserir(Venda venda);
+        Venda Instanciar(int codigo, int codStatusVenda, int codFormaPagamento, int codEndereco, int codUsuario);
+        Venda Editar(Venda venda);
     }
 }
---
+++ quasar/Quasar.Dominio/Vendas/Servicos/VendasServico.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quasar.Dominio.FormasPagamento.Entidades;
+using Quasar.Dominio.FormasPagamento.Servicos.Interfaces;
+using Quasar.Dominio.Produtos.Entidades;
+using Quasar.Dominio.Vendas.Entidades;
+using Quasar.Dominio.Vendas.Repositorios;
+using Quasar.Dominio.Vendas.Servicos.Interfaces;
+
+namespace Quasar.Dominio.Vendas.Servicos
+{
+    public class VendasServico : IVendasServico
+    {
+
+        private readonly IVendasRepositorio vendasRepositorio;
+        private readonly IStatusVendasServico statusVendaServico;
+        private readonly IFormasPagamentoServico formaPagamentoServico;
+
+        public VendasServico(IVendasRepositorio vendasRepositorio, IStatusVendasServico statusVendaServico, IFormasPagamentoServico formaPagamentoServico)
+        {
+            this.vendasRepositorio = vendasRepositorio;
+            this.statusVendaServico = statusVendaServico;
+            this.formaPagamentoServico = formaPagamentoServico;
+        }
+
+        public Venda Editar(Venda venda)
+        {   
+            Venda statusVendaEditar = Validar(venda.Codigo);
+
+            if (venda.StatusVenda != statusVendaEditar.StatusVenda)
+                statusVendaEditar.SetStatusVenda(venda.StatusVenda);
+
+            return vendasRepositorio.Editar(venda);
+        }
+
+        public Venda Inserir(Venda venda)
+        {
+            int codigo = vendasRepositorio.Inserir(venda);
+            venda.SetCodigo(codigo);
+            return venda;
+        }
+
+        public Venda Instanciar(int codigo, int codStatusVenda, int codFormaPagamento, int codEndereco, int codUsuario)
+        {
+            StatusVenda statusVenda = statusVendaServico.Validar(codStatusVenda);
+            FormaPagamento formaPagamento = formaPagamentoServico.Validar(codFormaPagamento);
+            Endereco endereco = enderecoServico.Validar(codEndereco);
+            Usuario usuario = usuarioServico.Validar(codUsuario);
+
+            Venda venda = new Venda(codigo, codStatusVenda, codFormaPagamento, codEndereco, codUsuario);
+            return venda;
+        }
+
+        public Venda Validar(int codigo)
+        {
+             Venda vendaValidar = vendasRepositorio.Recuperar(codigo);
+            if (vendaValidar == null)
+                throw new Exception("Venda não encontada.");
+            return vendaValidar;
+        }
+    }
+}
