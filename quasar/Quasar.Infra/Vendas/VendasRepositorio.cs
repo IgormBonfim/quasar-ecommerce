30ee77ac336569577d61592ce38e9ef4eacf6b04
Kevison Brandes <kevison.brande@autoglass.com.br>
2022-10-24 15:49:37 -0300

    Corrigindo algumas linhas de código

--- quasar/Quasar.Infra/Vendas/VendasRepositorio.cs
+++ quasar/Quasar.Infra/Vendas/VendasRepositorio.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NHibernate;
+using Quasar.Dominio.Vendas.Entidades;
+using Quasar.Dominio.Vendas.Repositorios;
+using Quasar.Infra.Genericos;
 
 namespace Quasar.Infra.Vendas
 {
-    public class VendasRepositorio
+    public class VendasRepositorio : GenericosRepositorio<Venda>, IVendasRepositorio
     {
-        
+        public VendasRepositorio(ISession session) : base(session)
+        {
+        }
     }
 }
