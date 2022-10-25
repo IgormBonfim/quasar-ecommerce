bb35d254ada5576245e36b8c02503943529f61f5
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-10-25 17:51:25 -0300

    Criando metodo inserir em controller

--- quasar/Quasar.API/Controllers/Carrinhos/CarrinhosController.cs
+++ quasar/Quasar.API/Controllers/Carrinhos/CarrinhosController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Quasar.Aplicacao.Carrinhos.Servicos.Interfaces;
+using Quasar.DataTransfer.Carrinhos.Requests;
 
 namespace Quasar.API.Controllers.Carrinhos
 {
@@ -10,6 +12,18 @@ namespace Quasar.API.Controllers.Carrinhos
     [Route("api/[controller]")]
     public class CarrinhosController : ControllerBase
     {
-        
+        private readonly ICarrinhosAppServico carrinhosAppServico;
+
+        public CarrinhosController(ICarrinhosAppServico carrinhosAppServico)
+        {
+            this.carrinhosAppServico = carrinhosAppServico;
+        }
+
+        [HttpPost]
+        public IActionResult Inserir([FromBody]CarrinhoInserirRequest inserirRequest)
+        {
+            var retorno = carrinhosAppServico.Inserir(inserirRequest);
+            return Ok(retorno);
+        }
     }
 }
