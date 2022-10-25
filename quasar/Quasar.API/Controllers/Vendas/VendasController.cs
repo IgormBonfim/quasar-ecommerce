2efcdbfbd29dcf07a6644998155e80493f32b453
Kevison Brandes <kevison.brande@autoglass.com.br>
2022-10-25 13:50:05 -0300

    Criando API da Venda

--- quasar/Quasar.API/Controllers/Vendas/VendasController.cs
+++ quasar/Quasar.API/Controllers/Vendas/VendasController.cs
@@ -2,11 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Quasar.Aplicacao.Vendas.Servicos.Interfaces;
+using Quasar.DataTransfer.Vendas.Request;
 
 namespace Quasar.API.Controllers.Vendas
 {
-    public class VendasController
+    [ApiController]
+    [Route("api/[venda]")]
+    public class VendasController : ControllerBase
     {
-        
+        private readonly IVendasAppServico vendasAppServico;
+        public VendasController(IVendasAppServico vendasAppServico)
+        {
+            this.vendasAppServico = vendasAppServico;
+        }
+    [HttpGet("codigo")]
+    public IActionResult Recuperar(int codigo)
+    {
+        var retorno = vendasAppServico.Recuperar(codigo)
+        return Ok(retorno);
+    }
+    [HttpPost]
+    public IActionResult Inserir([FromBody]VendaInserirRequest inserirRequest)
+    {
+        var retorno = vendasAppServico.Inserir(inserirRequest);
+        return Ok(retorno);
+    }
     }
 }
