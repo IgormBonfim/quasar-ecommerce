2d1740b07a53895e49b732b910e2abd24daa59c0
Kevison Brandes <kevison.brande@autoglass.com.br>
2022-10-25 17:26:23 -0300

    Criando Servico e repositorio de item venda. Ajustando código em outras classes.

--- quasar/Quasar.Dominio/Vendas/Servicos/ItensVendasServico.cs
+++ quasar/Quasar.Dominio/Vendas/Servicos/ItensVendasServico.cs
@@ -2,11 +2,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Quasar.Dominio.Produtos.Entidades;
+using Quasar.Dominio.Produtos.Servicos.Interfaces;
+using Quasar.Dominio.Vendas.Entidades;
+using Quasar.Dominio.Vendas.Repositorios;
+using Quasar.Dominio.Vendas.Servicos.Interfaces;
 
 namespace Quasar.Dominio.Vendas.Servicos
 {
-    public class ItensVendasServico
+    public class ItensVendasServico : IItensVendasServico
     {
-        
+        private readonly IItensVendasRepositorio itensVendasRepositorio;
+        private readonly IVendasServico vendasServico;
+        private readonly IProdutosServico produtoServico;
+
+        public ItensVendasServico(IItensVendasRepositorio itensVendasRepositorio, IVendasServico vendasServico, IProdutosServico produtoServico)
+        {
+            this.itensVendasRepositorio = itensVendasRepositorio;
+            this.vendasServico = vendasServico;
+            this.produtoServico = produtoServico;
+        }
+
+        public ItemVenda Inserir(Produto produto, ItemVenda itemVenda)
+        {
+            int codigo = itensVendasRepositorio.Inserir(itemVenda);
+            itemVenda.SetCodigo(codigo);
+            return itemVenda;
+        }
+        public ItemVenda Instanciar(int quantidade, int codVenda, int codProduto)
+        {
+            Venda venda = vendasServico.Validar(codVenda);
+            Produto produto = produtoServico.Validar(codProduto);
+
+            ItemVenda itemVenda = new ItemVenda(codVenda,
+                codProduto);
+            return itemVenda;
+        }
     }
 }
