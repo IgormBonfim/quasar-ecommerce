7393b1c6cfa206ae99ebb5d802a07f29a4117087
Igor Bonfim <igormbonfim@hotmail.com>
2022-10-27 17:25:41 -0300

    Adiciona metodos CRUD restantes

---
+++ quasar/Quasar.DataTransfer/Produtos/Requests/ProdutoEditarRequest.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.DataTransfer.Produtos.Requests
+{
+    public class ProdutoEditarRequest
+    {
+        public int? Codigo { get; set; }
+        public string? Descricao { get; set; }
+        public string? Nome { get; set; }
+        public decimal? Valor { get; set; }
+        public string? Imagem { get; set; }
+        public int? CodigoCategoria { get; set; }
+        public int? CodigoFornecedor { get; set; }
+        public EspecificacaoEditarRequest? Especificacao { get; set; }
+    }
+}
