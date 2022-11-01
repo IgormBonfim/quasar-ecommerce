834f8af1d284923c7b43aaa1c97d52812b21d702
Igor Bonfim <igormbonfim@hotmail.com>
2022-11-01 17:25:39 -0300

    101 - Adiciona Usuario logado

--- quasar/Quasar.DataTransfer/Categorias/Requests/CategoriaEditarRequest.cs
+++ quasar/Quasar.DataTransfer/Categorias/Requests/CategoriaEditarRequest.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Quasar.DataTransfer.Categorias.Requests
 {
     public class CategoriaEditarRequest
     {
+        [JsonIgnore]
         public int? Codigo {get; set;}
         public string? Nome {get; set;}
         public string? Imagem {get; set;}
