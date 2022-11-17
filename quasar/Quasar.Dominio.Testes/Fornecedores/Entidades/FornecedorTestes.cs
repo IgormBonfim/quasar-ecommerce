db61ca7302f73ab33b19e91da06ca6d633364790
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-11-17 17:52:24 -0300

    Fazendo os testes da entidade de fornecedor

---
+++ quasar/Quasar.Dominio.Testes/Fornecedores/Entidades/FornecedorTestes.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using Quasar.Dominio.Fornecedores.Entidades;
+using Xunit;
+
+namespace Quasar.Dominio.Testes.Fornecedores.Entidades
+{
+    public class FornecedorTestes
+    {
+        private readonly Fornecedor sut;
+        public FornecedorTestes()
+        {
+            sut = Builder<Fornecedor>.CreateNew().Build();
+        }
+
+        public class construtor
+        {
+            [Fact]
+            public void Quando_ParametrosForemValidos_Espero_ObjetoIntegro()
+            {
+                //Act
+                var fornecedor = new Fornecedor(
+                                    "Nome de um fornecedor",
+                                    "578330008",
+                                    "48567902000130",
+                                    "785955070"
+                                );
+
+                //Assert
+                fornecedor.Nome.Should().Be("Nome de um fornecedor");
+                fornecedor.RazaoSocial.Should().Be("578330008");
+                fornecedor.Cnpj.Should().Be("48567902000130");
+                fornecedor.Ie.Should().Be("785955070");
+            }
+        }
+
+        public class SetNomeMetodo : FornecedorTestes
+        {
+            [Theory]
+            [InlineData("")]
+            [InlineData(" ")]
+            [InlineData(null)]
+            public void Dado_NomeNuloOuEspacoEmBranco_Espero_Exception(string nome)
+            {
+                sut.Invoking(x => x.SetNome(new string(nome))).Should().Throw<Exception>();
+            }
+        }
+
+        public class SetRazaoSocialMetodo : FornecedorTestes
+        {
+            [Theory]
+            [InlineData("")]
+            [InlineData(" ")]
+            [InlineData(null)]
+            public void Dado_RazaoSocialNulaOuEspacoEmBranco_Espero_Exception(string razaoSocial)
+            {
+                sut.Invoking(x => x.SetRazaoSocial(new string(razaoSocial))).Should().Throw<Exception>();
+            }
+        }
+
+        public class SetCnpjMetodo : FornecedorTestes
+        {
+            [Theory]
+            [InlineData("")]
+            [InlineData(" ")]
+            [InlineData(null)]
+            public void Dado_CnpjNuloOuEspacoEmBranco_Espero_Exception(string cnpj)
+            {
+                sut.Invoking(x => x.SetCnpj(new string(cnpj))).Should().Throw<Exception>();
+            }
+        }
+
+        public class SetIeMetodo : FornecedorTestes
+        {
+            [Theory]
+            [InlineData("")]
+            [InlineData(" ")]
+            [InlineData(null)]
+            public void Dado_IeNuloOuEspacoEmBranco_Espero_Exception(string ie)
+            {
+                sut.Invoking(x => x.SetIe(new string(ie))).Should().Throw<Exception>();
+            }
+        }
+    }
+}
