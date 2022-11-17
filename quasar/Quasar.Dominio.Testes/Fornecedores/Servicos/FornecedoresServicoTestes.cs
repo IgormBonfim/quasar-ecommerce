cc4b1e272a74eefbf968c2a11083516a8e7255b6
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-11-17 17:52:45 -0300

    Criando o arquivo para os testes dos servicos de dominio de dornecedor

---
+++ quasar/Quasar.Dominio.Testes/Fornecedores/Servicos/FornecedoresServicoTestes.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Quasar.Dominio.Testes.Fornecedores.Servicos
+{
+    public class FornecedoresServicoTestes
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
