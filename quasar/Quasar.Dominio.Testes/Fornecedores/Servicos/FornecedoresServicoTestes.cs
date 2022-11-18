aae3fa59dd1479b28176dc910a48e1b3436e21fc
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-11-18 10:52:13 -0300

    criando o teste do metodo validar de fornecedor

--- quasar/Quasar.Dominio.Testes/Fornecedores/Servicos/FornecedoresServicoTestes.cs
+++ quasar/Quasar.Dominio.Testes/Fornecedores/Servicos/FornecedoresServicoTestes.cs
@@ -2,16 +2,49 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NSubstitute;
+using Quasar.Dominio.Fornecedores.Entidades;
+using Quasar.Dominio.Fornecedores.Repositorios;
+using Quasar.Dominio.Fornecedores.Servicos;
 using Xunit;
 
 namespace Quasar.Dominio.Testes.Fornecedores.Servicos
 {
     public class FornecedoresServicoTestes
     {
-        [Fact]
-        public void Test1()
+        private readonly FornecedoresServico sut;
+        private readonly IFornecedoresRepositorio fornecedoresRepositorio;
+
+        private readonly Fornecedor fornecedorValido;
+        
+        public FornecedoresServicoTestes()
         {
-            Assert.True(true);
+            fornecedorValido = Builder<Fornecedor>.CreateNew().Build();
+
+            fornecedoresRepositorio = Substitute.For<IFornecedoresRepositorio>();
+
+            sut = new FornecedoresServico(fornecedoresRepositorio);
+        }
+
+        public class ValidarMetodo : FornecedoresServicoTestes
+        {
+            [Fact]
+            public void Quando_FornecedorNaoEncontrado_Espero_Exception()
+            {
+                fornecedoresRepositorio.Recuperar(Arg.Any<int>()).Returns(x => null);
+
+                sut.Invoking(x => x.Validar(1)).Should().Throw<Exception>();
+            }
+
+            [Fact]
+            public void Quando_FornecedorEncontrado_Espero_Exception()
+            {
+                fornecedoresRepositorio.Recuperar(Arg.Any<int>()).Returns(fornecedorValido);
+
+                sut.Validar(1).Should().BeSameAs(fornecedorValido);
+            }
         }
     }
 }
