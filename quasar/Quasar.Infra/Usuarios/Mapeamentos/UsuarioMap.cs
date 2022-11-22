7a95e462441d900bffd204059a54ca673e66c709
Igor Bonfim <igormbonfim@hotmail.com>
2022-11-22 16:11:53 -0300

    127-AlteracaoORMUsuario
    Corrige o nome do mapeamento de usario para usuario

---
+++ quasar/Quasar.Infra/Usuarios/Mapeamentos/UsuarioMap.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentNHibernate.AspNetCore.Identity.Mappings;
+using FluentNHibernate.Mapping;
+using Quasar.Dominio.Usuarios.Entidades;
+
+namespace Quasar.Infra.Usuarios.Mapeamentos
+{
+    public class UsuarioMap : IdentityUserMapBase<Usuario, string>
+    {
+        public UsuarioMap() : base(u => u.Column("Id"))
+        {
+            Schema("quasarecommerce");
+            Table("Aspnetusers");
+            References(u => u.Cliente).Column("CodCliente");
+            HasManyToMany(x => x.Favoritos)
+            .Table("favorito")
+            .ParentKeyColumn("codUsuario")
+            .ChildKeyColumn("codProduto")
+            .NotFound.Ignore();
+        }
+    }
+}
