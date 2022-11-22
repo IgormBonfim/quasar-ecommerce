1f9c37f8b6f11c9da9e9d45b5bd43fbf64f1bd57
Igor Bonfim <igormbonfim@hotmail.com>
2022-11-22 16:10:36 -0300

    127-AlteracaoORMUsuario
    Adiciona metodo de login de usuario

--- quasar/Quasar.DataTransfer/Usuarios/Responses/UsuarioLoginResponse.cs
+++ quasar/Quasar.DataTransfer/Usuarios/Responses/UsuarioLoginResponse.cs
@@ -8,39 +8,22 @@ namespace Quasar.DataTransfer.Usuarios.Responses
 {
     public class UsuarioLoginResponse
     {
-        public bool Sucesso { get; private set; }
+        public bool Sucesso { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public string Token { get; private set; }
+        public string? Token { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public DateTime? DataExpiracao { get; private set; }
-        public List<string> Erros { get; private set; }
+        public string? Erro { get; set; }
 
-        public UsuarioLoginResponse()
-        {
-            Erros = new List<string>();
-        }
-
-        public UsuarioLoginResponse(bool sucesso = true) : this()
+        public UsuarioLoginResponse(bool sucesso = false)
         {
             Sucesso = sucesso;
         }
 
-        public UsuarioLoginResponse(bool sucesso, string token, DateTime dataExpiracao) : this(sucesso)
+        public UsuarioLoginResponse(bool sucesso, string token) : this(sucesso)
         {
             Token = token;
-            DataExpiracao = dataExpiracao;
-        }
-
-        public void AdicionarErro(string erro)
-        {
-            Erros.Add(erro);
-        }
-
-        public void AdicionarErros(IEnumerable<string> erros)
-        {
-            Erros.AddRange(erros);
         }
     }
 }
