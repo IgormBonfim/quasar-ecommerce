9125cddb1578d3bdd35bdd24665e97d6dcecd4b7
Igor Bonfim <igormbonfim@hotmail.com>
2022-11-22 16:26:34 -0300

    127-AlteracaoORMUsuario
    Altera o nome da propriedade de Codigo para Id em outros serviços

--- quasar/Quasar.Aplicacao/Vendas/Servicos/VendasAppServico.cs
+++ quasar/Quasar.Aplicacao/Vendas/Servicos/VendasAppServico.cs
@@ -12,7 +12,6 @@ using Quasar.Dominio.Genericos.Entidades;
 using Quasar.Dominio.Vendas.Entidades;
 using Quasar.Dominio.Vendas.Repositorios;
 using Quasar.Dominio.Vendas.Servicos.Interfaces;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Quasar.Aplicacao.Vendas.Servicos
 {
@@ -106,7 +105,7 @@ namespace Quasar.Aplicacao.Vendas.Servicos
 
             if (listarRequest.CodUsuario != null)
                 {
-                    query = query.Where(f => f.Usuario.Codigo == listarRequest.CodUsuario);
+                    query = query.Where(f => f.Usuario.Id == listarRequest.CodUsuario);
                 }
             ListaPaginada<Venda> listaVenda = vendasRepositorio.Listar(query, listarRequest.Quantidade, listarRequest.Pagina);
             return mapper.Map<ListaPaginadaResponse<VendaResponse>>(listaVenda);
