70b94e5923f03b725ba4bd2a36e0c30aa3441aa2
Aline_Kato <alineamy1612@gmail.com>
2022-11-23 14:42:31 -0300

    Teste de servico de categoria

---
+++ quasar/Quasar.Dominio.Testes/Categorias/Servico/CategoriasServicoTestes.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NSubstitute;
+using Quasar.Dominio.Categorias.Entidades;
+using Quasar.Dominio.Categorias.Repositorios;
+using Quasar.Dominio.Categorias.Servicos;
+using Quasar.Dominio.Categorias.Servicos.Interfaces;
+using Xunit;
+
+namespace Quasar.Dominio.Testes.Categorias.Servico
+{
+    public class CategoriasServicoTestes
+    {
+        private readonly CategoriasServico sut;
+        private readonly ICategoriasRepositorio categoriasRepositorio;
+        private readonly ICategoriasServico categoriaServico;
+        private readonly Categoria categoriaValida;
+
+        public CategoriasServicoTestes()
+        {
+            categoriaValida = Builder<Categoria>.CreateNew().Build();
+            categoriaServico = Substitute.For<ICategoriasServico>(); 
+        }
+        public class ValidarMetodo : CategoriasServicoTestes
+        {
+            [Fact]
+            public void Quando_CategoriaNaoForEncontrado_Espero_Exception()
+            {
+                categoriasRepositorio.Recuperar(Arg.Any<int>()).Returns(Xunit => null);
+                sut.Invoking(Xunit => Xunit.Validar(1)).Should().Throw<Exception>();
+            }
+            [Fact]
+            public void Quando_CategoriaForEncontrado_Espero_CategoriaValido()
+            {
+                categoriasRepositorio.Recuperar(Arg.Any<int>()).Returns(categoriaValida);
+                sut.Validar(1).Should().BeSameAs(categoriaValida);
+            }
+        }
+        public class EditarMetodo : CategoriasServicoTestes
+        {
+            public void Dado_ParametrosParaEditarCategoria_Espero_CategoriaEditado()
+            {
+                //Arrange
+                categoriasRepositorio.Recuperar(Arg.Any<int>()).Returns(categoriaValida);
+                categoriasRepositorio.Editar(Arg.Any<Categoria>()).Returns(categoriaValida);
+                var categoriaNovo = new Categoria(
+                                    "Teste de Nome da Categoria",
+                                    "Teste de Imagem da Categoria");
+                //Arc
+                var categoriaEditado = sut.Editar(categoriaNovo);
+                //Assert
+                categoriaEditado.Nome.Should().Be("Teste de Nome da Categoria");
+                categoriaEditado.Imagem.Should().Be("Teste de Imagem da Categoria");
+            }
+        }
+        public class InserirMetodo : CategoriasServicoTestes
+        {
+            [Fact]
+            public void Dado_CategoriaValida_Espero_CategoriaValido()
+            {
+                sut.Inserir(categoriaValida);
+                categoriasRepositorio.Received(1).Inserir(categoriaValida);
+            }
+        }
+        public class InstanciarMetodo :CategoriasServicoTestes
+        {
+            public void Dado_ParametrosParaCriarCategoria_Espero_CategoriaInstanciado()
+            {
+                var categoria = sut.Instanciar(
+                                "Teste de Nome da Categoria",
+                                "Teste de Imagem da Categoria");
+                categoria.Should().NotBeNull();
+            }
+        }
+    }
+}
