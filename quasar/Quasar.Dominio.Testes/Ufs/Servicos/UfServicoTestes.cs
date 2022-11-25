52a04fff7bfcb3642e3ba0c85efdfec1407ce733
Kevison Brandes <kevison.brandes@autoglass.com.br>
2022-11-25 17:51:31 -0300

    Retirei o metodo listar de serviços. Criei a classe de serviçostestes de UF

--- quasar/Quasar.Dominio.Testes/Ufs/Servicos/UfServicoTestes.cs
+++ quasar/Quasar.Dominio.Testes/Ufs/Servicos/UfServicoTestes.cs
@@ -2,11 +2,46 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NSubstitute;
+using Quasar.Dominio.Ufs.Entidades;
+using Quasar.Dominio.Ufs.Repositorios;
+using Quasar.Dominio.Ufs.Servicos;
+using Xunit;
 
 namespace Quasar.Dominio.Testes.Ufs.Servicos
 {
     public class UfServicoTestes
     {
-        
+        private readonly UfsServico sut;
+        private readonly IUfsRepositorio ufsRepositorio;
+        private readonly Uf ufValido;
+
+        public UfServicoTestes()
+        {
+            ufValido = Builder<Uf>.CreateNew().Build();
+            ufsRepositorio = Substitute.For<IUfsRepositorio>();
+
+            sut = new UfsServico(ufsRepositorio);
+
+        }
+        public class ValidarMetodo : UfServicoTestes
+        {
+            [Fact]
+            public void Quando_UfNaoForEncontrado_Espero_Exception()
+            {
+                ufsRepositorio.Recuperar(Arg.Any<int>()).Returns(x => null);
+
+                sut.Invoking(x => x.Validar(1)).Should().Throw<Exception>();
+            }
+
+            [Fact]
+            public void Quando_UfForEncontrado_Espero_ProdutoValido()
+            {
+                ufsRepositorio.Recuperar(Arg.Any<int>()).Returns(ufValido);
+                sut.Validar(1).Should().BeSameAs(ufValido);
+            }
+        }
     }
 }
