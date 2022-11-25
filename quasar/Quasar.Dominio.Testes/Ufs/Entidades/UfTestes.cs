ffecc36fccde23f6a411ae060790fd008e12ceb2
Kevison Brandes <kevison.brandes@autoglass.com.br>
2022-11-25 17:13:35 -0300

    Criei a classe de UfTestes e UfServicoTestes. Fiz toda a classe de entidade de UfTestes

---
+++ quasar/Quasar.Dominio.Testes/Ufs/Entidades/UfTestes.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using Quasar.Dominio.Ufs.Entidades;
+using Xunit;
+
+namespace Quasar.Dominio.Testes.Ufs.Entidades
+{
+    public class UfTestes
+    {
+        private readonly Uf sut;
+
+        public UfTestes()
+        {
+            sut = Builder<Uf>.CreateNew().Build();
+        }
+
+        public class Construtor
+        {
+            [Fact]
+            public void Quando_ParametrosForemValidos_Espero_ObjetoIntegro()
+            {
+                // Act
+                var uf = new Uf("TS",
+                                "Testes");    
+                //Assert
+                uf.Sigla.Should().Be("TS");
+                uf.Nome.Should().Be("Testes");       
+            }
+        }
+
+        public class SeSiglaMetodo : UfTestes
+        {
+            [Theory]
+            [InlineData(null)]
+            [InlineData("")]
+            [InlineData(" ")]
+            public void Dado_UfNegativa_Espero_Exception(string sigla)
+            {
+                sut.Invoking(x => x.SetSigla(sigla)).Should().Throw<Exception>();
+            }
+            [Fact]
+            public void Dado_UfComUmaLetra_Espero_Exception()
+            {
+                sut.Invoking(x => x.SetSigla(new string('*', 1))).Should().Throw<Exception>();
+            }
+            [Fact]
+            public void Dado_UfComTresLetras_Espero_Exception()
+            {
+                sut.Invoking(x => x.SetSigla(new string('*', 3))).Should().Throw<Exception>();
+            }
+        }
+
+        public class SetNomeMetodo : UfTestes
+        {
+            [Theory]
+            [InlineData(null)]
+            [InlineData("")]
+            [InlineData(" ")]
+            public void Dado_UfNegativa_Espero_Exception(string nome)
+            {
+                sut.Invoking(x => x.SetNome(nome)).Should().Throw<Exception>();
+            }
+            [Fact]
+            public void Dado_NomeDaUfComDuasLetra_Espero_Exception()
+            {
+                sut.Invoking(x => x.SetNome(new string('*', 2))).Should().Throw<Exception>();
+            }
+        }
+    }
+}
---
+++ quasar/Quasar.Dominio.Testes/Ufs/Servicos/UfServicoTestes.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quasar.Dominio.Testes.Ufs.Servicos
+{
+    public class UfServicoTestes
+    {
+        
+    }
+}
