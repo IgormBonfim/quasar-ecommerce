28f70aa1c67c8625a83998f37f1f0ca2e98b011e
Livia Pin <liviamtp@icloud.com>
2022-11-25 17:16:15 -0300

    #110- Teste de Cidade

---
+++ quasar/Quasar.Dominio.Testes/Cidades/Entidades/CidadeTestes.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FizzWare.NBuilder;
+using FluentAssertions;
+using Quasar.Dominio.Cidades.Entidades;
+using Quasar.Dominio.Ufs.Entidades;
+using Xunit;
+
+namespace Quasar.Dominio.Testes.Cidades.Entidades
+{
+    public class CidadeTestes
+    {
+        private readonly Cidade sut;
+        public CidadeTestes()
+        {
+            sut = Builder<Cidade>.CreateNew().Build();
+        }
+
+        public class Construtor
+        {
+            [Fact]
+            public void Quando_ParametrosForemValidos_Espero_ObjetoIntegro()
+            {
+                // Arrange
+                var uf = Builder<Uf>.CreateNew().Build();
+                
+                //Act
+                var cidade = new Cidade(
+                                "Nome de uma cidade",
+                                uf);
+                
+                
+                //Assert
+                cidade.Nome.Should().Be("Nome de uma cidade");
+                cidade.Uf.Should().BeSameAs(uf);
+            }
+        }
+
+        public class SetNomeMetodo : CidadeTestes
+        {
+            [Theory]
+            [InlineData("")]
+            [InlineData(" ")]
+            [InlineData(null)]
+            public void Dado_NomeNuloOuEspacoEmBranco_Espero_Exception(string nome)
+            {
+                sut.Invoking(x => x.SetNome(new string(nome))).Should().Throw<Exception>();
+            }
+
+            [Fact]
+            public void Dado_NomeComMenosDeTresCaracteres_Espero_Exception()
+            {
+                sut.Invoking(x => x.SetNome(new string('*', 2))).Should().Throw<Exception>();
+            }
+
+            [Fact]
+            public void Dado_NomeComMaisDeCemCaracteres_Espero_Exception()
+            {
+                sut.Invoking(x => x.SetNome(new string('*', 101))).Should().Throw<Exception>();
+            }
+
+            [Fact]
+            public void Dado_CidadeValido_Espero_PropriedadesPreenchidas()
+            {
+                sut.SetNome("Nome de uma cidade");
+                sut.Nome.Should().Be("Nome de uma cidade");
+            }
+            public class SetUfMetodo : CidadeTestes
+            {
+                [Theory]
+                [InlineData(null)]
+                public void Quando_UfForNulo_Espero_Exception(Uf uf)
+                {
+                    sut.Invoking(x => x.SetUf(uf)).Should().Throw<Exception>();
+                }
+
+                [Fact]
+                public void Quando_UfForValido_Espero_PropriedadePreenchida()
+                {
+                    var uf = Builder<Uf>.CreateNew().Build();
+                    sut.SetUf(uf);
+                    sut.Uf.Should().BeSameAs(uf);
+                }
+            }
+        }
+    }
+}
