520f51b57808751a6d9b48c75d30b17f47149977
Igor Bonfim <igormbonfim@hotmail.com>
2022-11-29 11:56:57 -0300

    # 113 Testes de Formas de Pagamento
    Adiciona testes dos metodos Validar e Instanciar do Serviço de Formas de Pagamento.

--- quasar/Quasar.Dominio.Testes/FormasPagamento/Servicos/FormasPagamentoServicoTestes.cs
+++ quasar/Quasar.Dominio.Testes/FormasPagamento/Servicos/FormasPagamentoServicoTestes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FizzWare.NBuilder;
+using FluentAssertions;
 using NSubstitute;
 using Quasar.Dominio.FormasPagamento.Entidades;
 using Quasar.Dominio.FormasPagamento.Repositorios;
@@ -25,6 +26,36 @@ namespace Quasar.Dominio.Testes.FormasPagamento.Servicos
             sut = new FormasPagamentoServico(formasPagamentoRepositorio);
 
             formaPagamentoValida = Builder<FormaPagamento>.CreateNew().Build();
+        }
+
+        public class ValidarMetodo : FormasPagamentoServicoTestes
+        {
+            [Fact]
+            public void Quando_FormaDePagamentoNaoForEncontrada_Espero_Exception()
+            {
+                formasPagamentoRepositorio.Recuperar(Arg.Any<int>()).Returns(x => null);
+
+                sut.Invoking(x => x.Validar(1)).Should().Throw<Exception>();
+            }
+
+            [Fact]
+            public void Quando_FomarDePagamentoForEncontrada_Espero_FormaDePagamentoValida()
+            {
+                formasPagamentoRepositorio.Recuperar(Arg.Any<int>()).Returns(formaPagamentoValida);
+
+                sut.Validar(1).Should().BeSameAs(formaPagamentoValida);
+            }
+        }
+
+        public class InstanciarMetodo: FormasPagamentoServicoTestes
+        {
+            [Fact]
+            public void Dado_ParametrosParaCriarFormaPagamento_Espero_FormaPagamentoInstanciada()
+            {
+                var formaPagamento = sut.Instanciar("Descrição da forma de pagamento");
+
+                formaPagamento.Should().NotBeNull();
+            }
         }
     }
 }
