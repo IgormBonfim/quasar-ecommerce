025e4576a526a463469133d87e44de1cbe707bd6
Igor Bonfim <igormbonfim@hotmail.com>
2022-12-13 13:18:15 -0300

    # 156 Tela de Produto
    Adiciona informações do produto

--- quasar-front/src/app/app.module.ts
+++ quasar-front/src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { CoreModule } from './core/core.module';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SharedModule } from './shared/shared.module';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AccordionModule } from 'primeng/accordion';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccordionModule } from 'primeng/accordion';
-import { MenuItem } from 'primeng/api';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common'
+
+registerLocaleData(ptBr)
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import { MenuItem } from 'primeng/api';
     SharedModule,
     CoreModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
