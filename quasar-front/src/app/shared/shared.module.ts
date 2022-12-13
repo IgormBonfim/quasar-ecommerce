c95512d8da3cf0324cd12d06ade2ee88d642912d
Igor Bonfim <igormbonfim@hotmail.com>
2022-12-13 16:31:45 -0300

    # 172 Configurações Frontend
    Adiciona configurações ao angular

--- quasar-front/src/app/shared/shared.module.ts
+++ quasar-front/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 import { InputTextModule } from 'primeng/inputtext'
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     CommonModule,
     InputTextModule,
-    RouterModule
+    RouterModule,
+    FontAwesomeModule
   ],
   exports: [
     HeaderComponent
