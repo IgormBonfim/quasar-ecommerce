1157ea45f2e37446345e8dbd3c116910a181112f
Aline_Kato <alineamy1612@gmail.com>
2022-12-16 16:26:03 -0300

    adicionei carouselcomponent e categoriaComponent

--- quasar-front/src/app/core/core.module.ts
+++ quasar-front/src/app/core/core.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './paginas/home/home.component';
+import { CarouselBannersComponent } from './components/carousel-banners/carousel-banners.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CarouselBannersComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
+    CarouselModule
   ],
   exports: [
     HomeComponent
