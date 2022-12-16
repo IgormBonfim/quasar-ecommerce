87942e99550bc0707231052940bd67850bb77709
Aline_Kato <alineamy1612@gmail.com>
2022-12-16 16:26:03 -0300

    adicionei carouselcomponent e categoriaComponent

--- quasar-front/src/app/shared/shared.module.ts
+++ quasar-front/src/app/shared/shared.module.ts
@@ -1,24 +1,29 @@
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { InputTextModule } from 'primeng/inputtext';
 
-
-import { InputTextModule } from 'primeng/inputtext'
+import { CategoriaComponent } from './components/categoria/categoria.component';
 import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    CategoriaComponent,
   ],
   imports: [
     CommonModule,
     InputTextModule,
     RouterModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    CategoriaComponent
   ]
 })
 export class SharedModule { }
