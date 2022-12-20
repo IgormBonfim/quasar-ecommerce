9a3f9aaf8688a8f8446f020aba9e3d8b08a9b59f
Igor Bonfim <igormbonfim@hotmail.com>
2022-12-20 15:21:41 -0300

    # 173 Component Card de Produto
    Adiciona servico de carrinho

--- quasar-front/src/app/shared/shared.module.ts
+++ quasar-front/src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
@@ -19,7 +20,8 @@ import { IconeCoracaoComponent } from './components/icone-coracao/icone-coracao.
     CommonModule,
     InputTextModule,
     RouterModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   exports: [
     HeaderComponent,
