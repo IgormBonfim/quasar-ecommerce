c7538fd38b5e7973a080a7ea470f516b95001efb
Igor Bonfim <igormbonfim@hotmail.com>
2022-12-20 12:14:42 -0300

    # 173 Component Card de Produto
    Adiciona component visual do card de produto

--- quasar-front/src/app/shared/shared.module.ts
+++ quasar-front/src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { InputTextModule } from 'primeng/inputtext';
 
-
-import { InputTextModule } from 'primeng/inputtext'
 import { HeaderComponent } from './components/header/header.component';
+import { ProdutoCardComponent } from './components/produto-card/produto-card.component';
+import { IconeCoracaoComponent } from './components/icone-coracao/icone-coracao.component';
+
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    ProdutoCardComponent,
+    IconeCoracaoComponent,
   ],
   imports: [
     CommonModule,
@@ -18,7 +22,9 @@ import { HeaderComponent } from './components/header/header.component';
     FontAwesomeModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    ProdutoCardComponent,
+    IconeCoracaoComponent
   ]
 })
 export class SharedModule { }
