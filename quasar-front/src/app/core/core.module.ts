2e4f241428248d1d9037fccd4c84d2a699cf5c7d
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-12-22 17:10:16 -0300

    Adicionandoa alterações visuais na tela

--- quasar-front/src/app/core/core.module.ts
+++ quasar-front/src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 
 import { SharedModule } from '../shared/shared.module';
