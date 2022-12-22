c8290bbbff4c4e23c078d640a2b159d9eeb7da9c
Aline_Kato <alineamy1612@gmail.com>
2022-12-22 15:47:42 -0300

    todo

---
+++ quasar-front/src/app/vendas/components/vendas-etapas/vendas-etapas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vendas-etapas',
+  templateUrl: './vendas-etapas.component.html',
+  styleUrls: ['./vendas-etapas.component.css']
+})
+export class VendasEtapasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
