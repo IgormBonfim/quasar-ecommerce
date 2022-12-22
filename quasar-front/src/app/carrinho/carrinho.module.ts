4a6046c74761b568373687cacc166b0c6c19e084
Livia Pin <liviamtp@icloud.com>
2022-12-22 15:51:34 -0300

    #157- Tela de Carrinho Finalização da estilização da tela.

--- quasar-front/src/app/carrinho/carrinho.module.ts
+++ quasar-front/src/app/carrinho/carrinho.module.ts
@@ -1,9 +1,10 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarrinhoRoutingModule } from './carrinho-routing.module';
 import { CarrinhoComponent } from './paginas/carrinho/carrinho.component';
 import { BoxCarrinhoSelecionadosComponent } from './components/box-carrinho-selecionados/box-carrinho-selecionados.component';
-import { FavoritadosComponent } from './components/favoritados/favoritados.component';
 import { FinalizacaoOutrosComponent } from './components/finalizacao-outros/finalizacao-outros.component';
 
 
@@ -11,12 +12,13 @@ import { FinalizacaoOutrosComponent } from './components/finalizacao-outros/fina
   declarations: [
     CarrinhoComponent,
     BoxCarrinhoSelecionadosComponent,
-    FavoritadosComponent,
     FinalizacaoOutrosComponent
   ],
   imports: [
     CommonModule,
-    CarrinhoRoutingModule
+    CarrinhoRoutingModule,
+    SharedModule,
+    FontAwesomeModule
   ]
 })
 export class CarrinhoModule { }
--- quasar-front/src/app/shared/shared.module.ts
+++ quasar-front/src/app/shared/shared.module.ts
@@ -2,23 +2,28 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {InputNumberModule} from 'primeng/inputnumber';
 
 
 import { InputTextModule } from 'primeng/inputtext'
 import { HeaderComponent } from './components/header/header.component';
+import { InputQuantidadeComponent } from './components/input-quantidade/input-quantidade.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    InputQuantidadeComponent,
   ],
   imports: [
     CommonModule,
     InputTextModule,
     RouterModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    InputNumberModule
   ],
   exports: [
-    HeaderComponent
-  ]
+    HeaderComponent,
+    InputQuantidadeComponent
+   ]
 })
 export class SharedModule { }
