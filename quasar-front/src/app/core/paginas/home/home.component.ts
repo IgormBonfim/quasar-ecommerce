160c90c89379adf9f72f4c1819ca12338a4321d6
Igor Bonfim <igormbonfim@hotmail.com>
2022-12-22 12:52:20 -0300

    # 173 Component Card Produto
    Remove card produto da home

--- quasar-front/src/app/core/paginas/home/home.component.ts
+++ quasar-front/src/app/core/paginas/home/home.component.ts
@@ -1,6 +1,4 @@
-import { ProdutoResponse } from './../../../shared/models/responses/produto.response';
 import { Component, OnInit } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +6,6 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  public faCoffee = faCoffee;
-  public produto = new ProdutoResponse();
 
   constructor() { }
 
