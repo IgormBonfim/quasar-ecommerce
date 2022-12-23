b6bf3116d5de2470c14a49d6e409b627295c0440
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-12-23 13:51:04 -0300

    Criando a base do servico de listagem de produtos

--- quasar-front/src/app/produtos/services/produtos.service.ts
+++ quasar-front/src/app/produtos/services/produtos.service.ts
@@ -1,3 +1,4 @@
+import { PaginacaoResponse } from 'src/app/shared/models/responses/paginacao.response';
 import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -10,9 +11,14 @@ import { ProdutoResponse } from 'src/app/shared/models/responses/produto.respons
 export class ProdutosService {
   baseUrl = environment.apiBaseUrl + 'produtos';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private readonly httpService: HttpClient) {}
 
-  listarProdutos() {
-    return this.httpClient.get<Observable<ProdutoResponse>>;
+  listarProdutos(
+    params: PaginacaoRequest<ProdutoResponse>
+  ): Observable<PaginacaoResponse<ProdutoResponse>> {
+    return this.httpService.get<Array<ProdutoResponse>>(this.baseUrl, {
+      params: params as any,
+      observe: 'response',
+    });
   }
 }
