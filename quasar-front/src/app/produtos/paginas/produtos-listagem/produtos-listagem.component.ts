dcd43c9f1de4ecc82a1ecdba91d58f4ecc7a893d
gabrielaraujo <gabrielaraujolira12@gmail.com>
2022-12-23 17:23:37 -0300

    Criando o servico de listar os produtos

--- quasar-front/src/app/produtos/paginas/produtos-listagem/produtos-listagem.component.ts
+++ quasar-front/src/app/produtos/paginas/produtos-listagem/produtos-listagem.component.ts
@@ -3,7 +3,8 @@ import { ProdutoResponse } from './../../../shared/models/responses/produto.resp
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { faSlidersH } from '@fortawesome/free-solid-svg-icons';
-// import { PaginacaoResponse } from 'src/app/shared/models/responses/paginacao.response';
+import { PaginacaoRequest } from 'src/app/shared/models/requests/paginacao.request';
+import { PaginacaoResponse } from 'src/app/shared/models/responses/paginacao.response';
 @Component({
   selector: 'app-produtos-listagem',
   templateUrl: './produtos-listagem.component.html',
@@ -12,17 +13,16 @@ import { faSlidersH } from '@fortawesome/free-solid-svg-icons';
 export class ProdutosListagemComponent implements OnInit {
   public filtro = faSlidersH;
 
-  // public request = new PaginacaoRequest<ProdutoResponse>({});
-  // public produto!: PaginacaoResponse<ProdutoResponse>;
+  public request = new PaginacaoRequest({});
+  public produtos!: PaginacaoResponse<ProdutoResponse>;
 
   produtoCard = new ProdutoResponse();
 
   constructor(private readonly produtosService: ProdutosService) {}
 
   ngOnInit(): void {
-    // this.recuperarProdutos();
+    this.recuperarProdutos();
   }
-  /*
   recuperarProdutos() {
     this.produtosService.listarProdutos(this.request).subscribe((produtos) => {
       this.produtos = produtos;
@@ -30,13 +30,12 @@ export class ProdutosListagemComponent implements OnInit {
   }
 
   trocarPagina(pagina: number) {
-    this.request.Pg = pagina;
+    this.request.pagina = pagina;
     this.recuperarProdutos();
   }
 
   trocarQuantidade(quantidade: number) {
-    this.request.Qt = quantidade;
+    this.request.quantidade = quantidade;
     this.recuperarProdutos();
   }
-  */
 }
--- quasar-front/src/app/produtos/services/produtos.service.ts
+++ quasar-front/src/app/produtos/services/produtos.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProdutoResponse } from 'src/app/shared/models/responses/produto.response';
+import { PaginacaoRequest } from 'src/app/shared/models/requests/paginacao.request';
 
 @Injectable({
   providedIn: 'root',
@@ -14,11 +15,19 @@ export class ProdutosService {
   constructor(private readonly httpService: HttpClient) {}
 
   listarProdutos(
-    params: PaginacaoRequest<ProdutoResponse>
+    params: PaginacaoRequest
   ): Observable<PaginacaoResponse<ProdutoResponse>> {
-    return this.httpService.get<Array<ProdutoResponse>>(this.baseUrl, {
-      params: params as any,
-      observe: 'response',
-    });
+    return this.httpService.get<PaginacaoResponse<ProdutoResponse>>(
+      this.baseUrl,
+      {
+        params: params as any,
+      }
+    );
+  }
+
+  recuperar(CodigoProduto: number): Observable<ProdutoResponse> {
+    return this.httpService.get<ProdutoResponse>(
+      `${this.baseUrl}/${CodigoProduto}`
+    );
   }
 }
