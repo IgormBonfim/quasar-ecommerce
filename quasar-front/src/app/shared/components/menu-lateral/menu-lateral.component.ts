203da320b20575b2db3b5f515559ced3a0044617
Livia Pin <liviamtp@icloud.com>
2022-12-26 16:49:25 -0300

    #170- Botão Menu- Sidebar

--- quasar-front/src/app/shared/components/menu-lateral/menu-lateral.component.ts
+++ quasar-front/src/app/shared/components/menu-lateral/menu-lateral.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-lateral',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-lateral.component.css']
 })
 export class MenuLateralComponent implements OnInit {
-  visibilidade = false;
+  @Input()
+  public visibilidade = false;
 
   constructor() { }
 
