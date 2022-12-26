2107ba85e2c0d14d027cda1d0dbe5941fb81bca0
Livia Pin <livia.pin@autoglass.com.br>
2022-12-26 16:49:25 -0300

    #170- Botão Menu- Sidebar

--- quasar-front/src/app/shared/components/header/header.component.ts
+++ quasar-front/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   constructor() { }
+
+  public visibilidade: boolean = false;
 
   ngOnInit(): void {
   }
