9f46deee33550d63bedeb41a7cce442990d4262c
Igor Bonfim <igormbonfim@hotmail.com>
2022-12-27 17:58:59 -0300

    # 186 Tela Novo Produto
    Adiciona tela de adicionar produto

--- quasar-front/src/app/produtos/produtos.module.ts
+++ quasar-front/src/app/produtos/produtos.module.ts
@@ -1,18 +1,24 @@
-import { SharedModule } from './../shared/shared.module';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ProdutosRoutingModule } from './produtos-routing.module';
-import { ProdutosListagemComponent } from './paginas/produtos-listagem/produtos-listagem.component';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DropdownModule } from 'primeng/dropdown';
+
+import { SharedModule } from './../shared/shared.module';
+import { ProdutosAdicionarComponent } from './paginas/produtos-adicionar/produtos-adicionar.component';
+import { ProdutosListagemComponent } from './paginas/produtos-listagem/produtos-listagem.component';
+import { ProdutosRoutingModule } from './produtos-routing.module';
+
 
 @NgModule({
-  declarations: [ProdutosListagemComponent],
+  declarations: [ProdutosListagemComponent, ProdutosAdicionarComponent],
   imports: [
     CommonModule,
     ProdutosRoutingModule,
     SharedModule,
     FontAwesomeModule,
+    DropdownModule,
+    ReactiveFormsModule
   ],
 })
 export class ProdutosModule {}
