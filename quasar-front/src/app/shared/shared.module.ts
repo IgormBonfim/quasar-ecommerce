a5c9a7fc52ab51eb59c61dc02c23938d827cc92d
Aline_Kato <alineamy1612@gmail.com>
2022-12-27 12:38:33 -0300

    conflito

--- quasar-front/src/app/shared/shared.module.ts
+++ quasar-front/src/app/shared/shared.module.ts
@@ -6,25 +6,19 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { InputTextModule } from 'primeng/inputtext';
 
 import { HeaderComponent } from './components/header/header.component';
-<<<<<<< HEAD
 import { FooterComponent } from './components/footer/footer.component';
-=======
 import { ProdutoCardComponent } from './components/produto-card/produto-card.component';
 import { IconeCoracaoComponent } from './components/icone-coracao/icone-coracao.component';
 import { BotaoAdicionarCarrinhoComponent } from './components/botao-adicionar-carrinho/botao-adicionar-carrinho.component';
 
->>>>>>> 87fda87f34fb790299ad8fad1fcaffdcc6131429
 
 @NgModule({
   declarations: [
     HeaderComponent,
-<<<<<<< HEAD
     FooterComponent,
-=======
     ProdutoCardComponent,
     IconeCoracaoComponent,
     BotaoAdicionarCarrinhoComponent,
->>>>>>> 87fda87f34fb790299ad8fad1fcaffdcc6131429
   ],
   imports: [
     CommonModule,
@@ -35,12 +29,8 @@ import { BotaoAdicionarCarrinhoComponent } from './components/botao-adicionar-ca
   ],
   exports: [
     HeaderComponent,
-<<<<<<< HEAD
-    FooterComponent
-=======
-    ProdutoCardComponent,
+    FooterComponent,
     IconeCoracaoComponent
->>>>>>> 87fda87f34fb790299ad8fad1fcaffdcc6131429
   ]
 })
-export class SharedModule { }
+ export class SharedModule{};
