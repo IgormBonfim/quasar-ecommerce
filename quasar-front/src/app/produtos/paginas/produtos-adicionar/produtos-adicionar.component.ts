ef2e58caaeffba89d105d4b6f7fff5408e4d4576
Igor Bonfim <igormbonfim@hotmail.com>
2022-12-27 17:58:59 -0300

    # 186 Tela Novo Produto
    Adiciona tela de adicionar produto

---
+++ quasar-front/src/app/produtos/paginas/produtos-adicionar/produtos-adicionar.component.ts
@@ -0,0 +1,69 @@
+import { ProdutoInserirRequest } from './../../models/requests/produtoInserir.request';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ProdutoInserirResponse } from './../../models/responses/produtoInserir.response';
+import { ProdutosService } from './../../services/produtos.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EspecificacaoInserirRequest } from '../../models/requests/especificacaoInserir.request';
+
+@Component({
+  selector: 'app-produtos-adicionar',
+  templateUrl: './produtos-adicionar.component.html',
+  styleUrls: ['./produtos-adicionar.component.css']
+})
+export class ProdutosAdicionarComponent implements OnInit {
+
+  produtoForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private produtosService: ProdutosService
+    ) { }
+
+  ngOnInit(): void {
+    this.produtoForm = this.formBuilder.group({
+      nome: ['', [Validators.required]],
+      descricao: ['', [Validators.required]],
+      imagem: ['', [Validators.required]],
+      posicao: ['', [Validators.required]],
+      cor: ['', [Validators.required]],
+      ano: ['', [Validators.required]],
+      modelo: ['', [Validators.required]],
+      quantidade: [0, [Validators.required]],
+      valor: ['', [Validators.required]],
+      categoria: [0, [Validators.required]],
+      fornecedor: [0, [Validators.required]],
+    })
+  }
+
+  botaoEnviar() {
+    let formulario = this.produtoForm.getRawValue();
+    console.log(formulario);
+
+    let especificacao = new EspecificacaoInserirRequest();
+
+    especificacao.ano = formulario.ano;
+    especificacao.cor = formulario.cor;
+    especificacao.posicao = formulario.posicao;
+    especificacao.veiculo = formulario.modelo;
+
+    let produto = new ProdutoInserirRequest();
+
+    produto.nome = formulario.nome;
+    produto.descricao = formulario.descricao;
+    produto.imagem = formulario.imagem;
+    produto.codigoCategoria = formulario.categoria;
+    produto.codigoFornecedor = formulario.fornecedor;
+    produto.especificacao = especificacao;
+
+    this.produtosService.adicionar(produto).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (erro: HttpErrorResponse) => {
+        console.log(erro);
+      }
+    });
+  }
+
+}
--- quasar-front/src/app/produtos/produtos-routing.module.ts
+++ quasar-front/src/app/produtos/produtos-routing.module.ts
@@ -1,12 +1,19 @@
+import { ProdutosAdicionarComponent } from './paginas/produtos-adicionar/produtos-adicionar.component';
 import { ProdutosListagemComponent } from './paginas/produtos-listagem/produtos-listagem.component';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { AuthGuard } from '../shared/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: "",
     pathMatch: 'full',
     component: ProdutosListagemComponent,
+  },
+  {
+    path: "adicionar",
+    component: ProdutosAdicionarComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
