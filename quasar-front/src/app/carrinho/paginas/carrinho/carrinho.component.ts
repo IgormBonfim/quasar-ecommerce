85e3c9ae61b56ce3131661083e071afeeb65bed5
Livia Pin <liviamtp@icloud.com>
2022-12-30 12:10:09 -0300

    #157

--- quasar-front/src/app/carrinho/paginas/carrinho/carrinho.component.ts
+++ quasar-front/src/app/carrinho/paginas/carrinho/carrinho.component.ts
@@ -1,4 +1,8 @@
+import { PaginacaoRequest } from './../../../shared/models/requests/paginacao.request';
+import { CarrinhosService } from './../../../shared/services/carrinhos.service';
+import { PaginacaoResponse } from 'src/app/shared/models/responses/paginacao.response';
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoResponse } from 'src/app/shared/models/responses/carrinho.response';
 
 @Component({
   selector: 'app-carrinho',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrinho.component.css']
 })
 export class CarrinhoComponent implements OnInit {
+  public produtosCarrinho!: PaginacaoResponse<CarrinhoResponse>;
+  public request = new PaginacaoRequest({
+    quantidade: 9999
+  })
 
-  constructor() { }
+
+  constructor(private readonly carrinhosService: CarrinhosService) { }
 
   ngOnInit(): void {
+    this.recuperarCarrinhos();
   }
 
+  recuperarCarrinhos(){
+    this.carrinhosService.listar(this.request).subscribe({
+      next: (res: PaginacaoResponse<CarrinhoResponse>) => {
+        this.produtosCarrinho = res;
+        console.log(res);
+      }
+    })
+  }
 }
