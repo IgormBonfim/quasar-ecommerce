e0a25d004ac02736c34d03e962a7495ef08d1fb1
Igor Bonfim <igormbonfim@hotmail.com>
2023-01-05 13:24:17 -0300

    # 156 Tela de Produto
    Adiciona icones

--- quasar-front/src/app/produtos/components/produto-info/produto-info.component.ts
+++ quasar-front/src/app/produtos/components/produto-info/produto-info.component.ts
@@ -1,5 +1,7 @@
-import { ProdutoResponse } from './../../../shared/models/responses/produto.response';
 import { Component, Input, OnInit } from '@angular/core';
+import { faCreditCard, faHeart, faMoneyBill, faShareNodes } from '@fortawesome/free-solid-svg-icons';
+
+import { ProdutoResponse } from './../../../shared/models/responses/produto.response';
 
 @Component({
   selector: 'app-produto-info',
@@ -7,6 +9,11 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./produto-info.component.css']
 })
 export class ProdutoInfoComponent implements OnInit {
+
+  public iconeCoracao = faHeart;
+  public iconeShare = faShareNodes;
+  public iconeDinheiro = faMoneyBill;
+  public iconeCartao = faCreditCard;
 
   @Input()
   public produtoDetalhe!: ProdutoResponse;
