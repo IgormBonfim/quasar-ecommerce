b8f534bb041dd731415514dd9d3a6e626fd5d7b2
gabrielaraujo <gabrielaraujolira12@gmail.com>
2023-01-06 17:37:25 -0300

    Criando o evento de pesquisa da header

--- quasar-front/src/app/shared/components/header/header.component.ts
+++ quasar-front/src/app/shared/components/header/header.component.ts
@@ -1,16 +1,24 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() { }
+  constructor(private readonly activatedRoute: ActivatedRoute) {}
 
   public visibilidade: boolean = false;
+  public nome?: string; //Nome do produto
 
-  ngOnInit(): void {
+  onKeydown(event: any) {
+    if (event.key === 'Enter') {
+      console.log(event);
+    }
   }
 
+  ngOnInit(): void {
+    this.nome = this.activatedRoute.snapshot.queryParams['nome'];
+  }
 }
