35c4e3f2935b72c3abdc06a42be907d1ce8202a5
Aline_Kato <alineamy1612@gmail.com>
2023-01-06 16:46:04 -0300

    user

---
+++ quasar-front/src/app/usuario/usuario.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UsuarioRoutingModule } from './usuario-routing.module';
+import { PerfilComponent } from './paginas/perfil/perfil.component';
+
+
+@NgModule({
+  declarations: [
+    PerfilComponent
+  ],
+  imports: [
+    CommonModule,
+    UsuarioRoutingModule
+  ]
+})
+export class UsuarioModule { }
