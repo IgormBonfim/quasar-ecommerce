a7fa23c2c27c9263f2656def5bf55d652feb19bb
Igor Bonfim <igormbonfim@hotmail.com>
2023-01-07 00:25:24 -0300

    # 186 Tela de Novo Produto
    Altera ordem do instanciar no backend e ajusta models para realizar a requisição da maneira correta.

--- quasar-front/src/app/produtos/models/requests/produtoInserir.request.ts
+++ quasar-front/src/app/produtos/models/requests/produtoInserir.request.ts
@@ -1,13 +1,23 @@
 import { EspecificacaoInserirRequest } from "./especificacaoInserir.request";
 
 export class ProdutoInserirRequest {
-  public descricao!: string;
-  public nome!: string;
-  public valor!: number;
-  public imagem!: string;
-  public codigoCategoria!: number;
-  public codigoFornecedor!: number;
-  public especificacao!: EspecificacaoInserirRequest;
+  public descricao: string;
+  public nome: string;
+  public valor: number;
+  public imagem: string;
+  public codigoCategoria: number;
+  public codigoFornecedor: number;
+  public especificacao: EspecificacaoInserirRequest;
+
+  constructor(params: Partial<ProdutoInserirRequest>) {
+    this.descricao = params.descricao || '';
+    this.nome = params.nome || '';
+    this.valor = params.valor || 0;
+    this.imagem = params.imagem || '';
+    this.codigoCategoria = params.codigoCategoria || 0;
+    this.codigoFornecedor = params.codigoFornecedor || 0;
+    this.especificacao = params.especificacao || new EspecificacaoInserirRequest({});
+  }
 }
 
 
