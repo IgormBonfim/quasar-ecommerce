71dc6633e0976eb26dcbb622a6a4c8eb226af328
Igor Bonfim <igormbonfim@hotmail.com>
2023-01-12 12:21:04 -0300

    # 169 - Finalização
    Corrige tela de perfil

--- quasar-front/src/app/usuario/paginas/perfil/perfil.component.ts
+++ quasar-front/src/app/usuario/paginas/perfil/perfil.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { UsuarioResponse } from 'src/app/shared/models/responses/usuario.response';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { UsuariosService } from 'src/app/shared/services/usuarios.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  iconeUsuario = faUserCircle;
+  public usuario!: UsuarioResponse;
+  public administrador: boolean = false;
+
+  constructor(
+    private usuarioService: UsuariosService,
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.recuperarDados();
   }
 
+  private recuperarDados() {
+    let codigoUsuario: string = this.authService.recuperarCodigo();
+
+    this.usuarioService.recuperar(codigoUsuario).subscribe({
+      next: (res: UsuarioResponse) => {
+        this.usuario = res;
+        this.administrador = res.cliente.nome == "Quasar E-commerce"
+      }
+    })
+  }
+
+  public botaoLogout() {
+    this.authService.logout();
+    this.router.navigate(['/home']);
+  }
 }
