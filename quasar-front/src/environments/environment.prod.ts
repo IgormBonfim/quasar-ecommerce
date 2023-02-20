236927e487029ee3891f7f4c87c83f217ec1f67e
IgorBonfim <igormbonfim@hotmail.com>
2023-02-20 10:31:58 -0300

    Ajusta URL da api no front-end

--- quasar-front/src/environments/environment.prod.ts
+++ quasar-front/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: "https://quasar-ecommerce.up.railway.app/api/"
 };
--- quasar/Quasar.API/Program.cs
+++ quasar/Quasar.API/Program.cs
@@ -15,11 +15,10 @@ builder.Services.InjetarDependencias(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseCors();
 
